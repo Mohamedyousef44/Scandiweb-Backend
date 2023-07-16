4bbbef3b1e2a43592a8b6a47bf54aa8a76cda992
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-07-16 12:40:28 +0300

    create class for response has all response status code

---
+++ Core/Response.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Core;
+
+class Response
+{
+    public const BAD_REQUEST = 400;
+    public const NOT_FOUND = 404;
+    public const FORBIDDEN = 403;
+    public const INTERNAL_ERROR = 500;
+    public const CONTENT_TYPE = 'Content-Type: application/json';
+
+}
